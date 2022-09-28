10bdb3cebac03e270b11ceba0ea4850762ce7d67
OldTyT <nezlobin118@gmail.com>
2022-09-28 20:51:07 +0800

    fix github actions

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	ErrorLogger   *log.Logger
 )
 
-var Version string = "v0.0.2"
+var Version string = "v0.0.4"
 var URLServer string = "None"
 var LogPath string = "/var/log/webhook.executor.log"
 
